b1a691596e3892b154185fc64bc8e82024321d06
Adir Havakuk <135220234+adir-001@users.noreply.github.com>
2024-12-08 20:15:40 +0200

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("new new new " + i);
+            System.out.println("new new " + i);
         }
     }
 }
