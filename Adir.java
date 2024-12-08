343bb746b4e25a665d1b0c06ecb03373e419ced4
Adir Havakuk <135220234+adir-001@users.noreply.github.com>
2024-12-08 20:11:16 +0200

    Update Adir.java
    change 01

--- Adir.java
+++ Adir.java
@@ -1,7 +1,7 @@
 public class Adir {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("adir12345 "+ i);
+            System.out.println("Shay "+ i);
         }
     }
 }
