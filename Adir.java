0c1b875e0bca36310d2fc03886593a3ff7a7a8b5
Adir Havakuk <135220234+adir-001@users.noreply.github.com>
2024-12-08 20:08:58 +0200

    Update Adir.java
    jhfdsjfsjf

--- Adir.java
+++ Adir.java
@@ -1,7 +1,7 @@
 public class Adir {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Adir "+ i);
+            System.out.println("adir12345 "+ i);
         }
     }
 }
